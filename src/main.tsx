0c05b2a0b10037c0ca1373f384dea7de66d6ab06
andresdlt <andelatmo@gmail.com>
2023-11-29 21:54:05 +0100

    add react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import './sass/app.scss'
+import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { ContactPage, LandingPage, TeamPage } from './pages'
+import { Layout } from './layouts'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        element: <LandingPage />,
+        index: true,
+      },
+      {
+        element: <ContactPage />,
+        path: "/contact",
+      },
+      {
+        element: <TeamPage />,
+        path: "/team",
+      },
+      {
+        element: <Navigate to="/"/>,
+        path: "*"
+      }
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

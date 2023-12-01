7983016dc2c1e1c62c4a8312e7a8b53b67201b92
andresdlt <andelatmo@gmail.com>
2023-12-01 16:43:17 +0100

    add titled text box in landing page

--- src/components/TitledTextBox.tsx
+++ src/components/TitledTextBox.tsx
@@ -1,8 +1,15 @@
+interface TitledTextBoxProps {
+    title: string;
+    text: string;
+}
 
-export const TitledTextBox = () => {
+export const TitledTextBox: React.FC<TitledTextBoxProps> = ({ title, text }) => {
   return (
-    <>
-
-    </>
+    <div className="titled-text-box">
+        <h2 className="titled-text-box__title">{title}</h2>
+        <div className="titled-text-box__box">
+            <p className="titled-text-box__text">{text}</p>
+        </div>
+    </div>
   )
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,3 +1,4 @@
+import { TitledTextBox } from "../components"
 
 export const LandingPage = () => {
   return (
@@ -7,16 +8,26 @@ export const LandingPage = () => {
       </div>
 
       <section className="landing__section section-2">
-
+        <div className="landing__titled-text-box-1">
+          <TitledTextBox 
+            title="¿Qué hacemos?"
+            text="Nuestra trayectoria de casi una década destaca por una combinación única de innovación, creatividad y dedicación. Cada temporada nos enfrentamos al enorme reto de crear un vehículo nuevo, incorporando nuevas ideas y mejorando año a año. Para ello trabajamos en estrecha colaboración con las empresas que nos apoyan, aprovechando la diversidad de talento y perspectivas que tenemos para abordar de forma efectiva los desafíos técnicos y logísticos que presenta hyperloop."
+          />
+        </div>
       </section>
 
       <section className="landing__section section-3">
-
+        <div className="landing__titled-text-box-2">
+          <TitledTextBox 
+            title="¿Quién somos?"
+            text="En Hyperloop UPV somos un equipo de 50 estudiantes de múltiples disciplinas y áreas de estudio de la Universitat Politècnica de València unidos con el objetivo de diseñar, implementar y perfeccionar la tecnología hyperloop."
+          />
+        </div>
       </section>
 
-      <video src="/assets/backgrounds/landing-4-video.jpeg">
+      {/* <video src="/assets/backgrounds/landing-4-video.jpeg">
 
-      </video>
+      </video> */}
 
       <section className="landing__section section-5">
         

2eda161e37e7cc790dbe33aa8d5636be1052bd62
jmaralo <martinezalonsojn@gmail.com>
2023-12-06 16:19:43 +0100

    Add about page route

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './sass/app.scss'
 import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { ContactPage, LandingPage, TeamPage } from './pages'
+import { AboutPage, ContactPage, LandingPage, TeamPage } from './pages'
 import { Layout } from './layout'
 
 const router = createBrowserRouter([
@@ -13,6 +13,10 @@ const router = createBrowserRouter([
       {
         element: <LandingPage />,
         index: true,
+      },
+      {
+        element: <AboutPage />,
+        path: "/about",
       },
       {
         element: <ContactPage />,

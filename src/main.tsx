c401102218c02db034e53654770724e01b730ed0
jmaralo <martinezalonsojn@gmail.com>
2023-12-18 15:55:16 +0100

    Add error page when route is not recognized

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './sass/app.scss'
-import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { AboutPage, ContactPage, LandingPage, PartnersPage, TeamPage } from './pages'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { AboutPage, ContactPage, ErrorPage, LandingPage, PartnersPage, TeamPage } from './pages'
 import { Layout } from './layout'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         element: <LandingPage />,
@@ -28,18 +29,16 @@ const router = createBrowserRouter([
       },
       {
         element: <PartnersPage />,
-        path: "/partners"
+        path: "/partners",
       },
-      {
-        element: <Navigate to="/" />,
-        path: "*"
-      }
     ],
   },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider
+      router={router}
+    />
   </React.StrictMode>,
 )

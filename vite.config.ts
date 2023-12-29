9b5a8ddeb841e241f517de70c4f7170022a81fc8
andresdlt <andelatmo@gmail.com>
2023-12-29 10:21:46 +0100

    add image minifier

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: "webpage",
   },
-  plugins: [react()],
+  plugins: [
+    react(), 
+    ViteImageOptimizer()
+  ],
 })

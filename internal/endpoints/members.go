140c1b599b4889f2db19f36e87aa1f107b9f72fa
jmaralo <martinezalonsojn@gmail.com>
2024-03-06 19:50:17 +0100

    Add post method to update members

--- internal/endpoints/members.go
+++ internal/endpoints/members.go
@@ -2,7 +2,9 @@ package endpoints
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/HyperloopUPV-H8/webpage-backend/internal/members"
@@ -11,6 +13,8 @@ import (
 )
 
 var _ http.Handler = &Members{}
+
+const MemberMediaFolder = "/media/members"
 
 type Members struct {
 	lastUpdated time.Time
@@ -28,6 +32,8 @@ func (endpoint *Members) ServeHTTP(writter http.ResponseWriter, request *http.Re
 	switch request.Method {
 	case http.MethodGet:
 		endpoint.get(writter, request)
+	case http.MethodPost:
+		endpoint.post(writter, request)
 	default:
 		log.Warn().Str("method", request.Method).Msg("method not allowed")
 		request.Body.Close()
@@ -58,4 +64,62 @@ func (endpoint *Members) get(writter http.ResponseWriter, request *http.Request)
 			return
 		}
 	}
+}
+
+func (endpoint *Members) post(_ http.ResponseWriter, request *http.Request) {
+	defer request.Body.Close()
+	log.Debug().Msg("post")
+
+	var updates []SubsystemUpdate
+	err := json.NewDecoder(request.Body).Decode(&updates)
+	if err != nil {
+		log.Error().Stack().Err(err).Msg("decode body")
+	}
+
+	newSubsystems := make([]members.Subsystem, len(updates))
+	for i, update := range updates {
+		newSubsystems[i] = update.toSubsystem()
+	}
+
+	endpoint.lastUpdated = time.Now()
+	endpoint.subsystems = newSubsystems
+}
+
+type SubsystemUpdate struct {
+	Name    string         `json:"name"`
+	Members []MemberUpdate `json:"members"`
+}
+
+func (update SubsystemUpdate) toSubsystem() members.Subsystem {
+	newMembers := make([]members.Member, len(update.Members))
+	for i, memberUpdate := range update.Members {
+		newMembers[i] = memberUpdate.toMember()
+	}
+	return members.Subsystem{
+		Name:    update.Name,
+		Members: newMembers,
+	}
+}
+
+type MemberUpdate struct {
+	Name       string `json:"name"`
+	Role       string `json:"role"`
+	SocialsURL string `json:"socialsURL"`
+}
+
+func (update MemberUpdate) toMember() members.Member {
+	return members.Member{
+		Name:       update.Name,
+		ImageURL:   getImagePath(update.Name),
+		Role:       update.Role,
+		SocialsURL: update.SocialsURL,
+	}
+}
+
+func getImagePath(name string) string {
+	return fmt.Sprintf("%s/%s.webp", MemberMediaFolder, formatName(name))
+}
+
+func formatName(name string) string {
+	return strings.Join(strings.Split(strings.ToLower(name), " "), "_")
 }

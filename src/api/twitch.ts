0d2b6fdb4cdad28e6e184f4cf8d907452a30d6c0
andresdlt03 <andelatmo@gmail.com>
2024-11-21 12:56:42 +0100

    add api interaction logic

---
+++ src/api/twitch.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const CLIENT_ID = '3sfbmzpgqg9e6ezfsooo5ug48rjx0u';
+const CHANNEL_NAME = "HyperloopUPV"
+
+
+async function isHyperloopUPVOnline(): Promise<boolean> {
+  try {
+    const response = await axios.get('https://api.twitch.tv/helix/streams', {
+      headers: {
+        'Client-ID': CLIENT_ID,
+      },
+      params: {
+        user_login: CHANNEL_NAME,
+      },
+    });
+
+    const streams = response.data.data;
+    return streams.length > 0; 
+  } catch (error) {
+    console.error('Error verificando el estado del canal:');
+    return false;
+  }
+}

132cabadbab24a1b961720cce8bb65483154a468
Joan Física <joanfisica5@gmail.com>
2025-08-05 19:42:20 +0200

    Place newer FRS first

--- src/pages/ResearchPage/index.tsx
+++ src/pages/ResearchPage/index.tsx
@@ -20,10 +20,10 @@ export default function ResearchPage() {
             </div>
             <div className={style.section_2}>
                 <FileButton
-                    fileTitle="Technical FRS H8"
-                    fileDesc="Building a Sense & Control testing environment for the development of a hyperloop vehicle"
-                    fileName="Technical FRS H8.pdf"
-                    fileURL={techH8}
+                    fileTitle="Technical FRS H10"
+                    fileDesc="Comparative analysis of energy supply systems for hyperloop vehicle implementation"
+                    fileName="Technical FRS H10.pdf"
+                    fileURL={techH10}
                     viewportParameters={{ scale: 1.0 }}
                     previewWidth={210 * 2}
                     previewHeight={297 * 2}
@@ -47,10 +47,10 @@ export default function ResearchPage() {
                     previewHeight={297 * 2}
                 />
                 <FileButton
-                    fileTitle="Technical FRS H10"
-                    fileDesc="Comparative analysis of energy supply systems for hyperloop vehicle implementation"
-                    fileName="Technical FRS H10.pdf"
-                    fileURL={techH10}
+                    fileTitle="Technical FRS H8"
+                    fileDesc="Building a Sense & Control testing environment for the development of a hyperloop vehicle"
+                    fileName="Technical FRS H8.pdf"
+                    fileURL={techH8}
                     viewportParameters={{ scale: 1.0 }}
                     previewWidth={210 * 2}
                     previewHeight={297 * 2}

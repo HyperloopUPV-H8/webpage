84dbea735b1069945b1a9ce83ef21b4bd4896ab0
Joan Física <joanfisica5@gmail.com>
2025-08-05 19:40:05 +0200

    Add technical FRS H10

--- src/pages/ResearchPage/index.tsx
+++ src/pages/ResearchPage/index.tsx
@@ -1,6 +1,7 @@
 import techH8 from '../../assets/documents/frs-tech-h8.pdf';
 import techH9 from '../../assets/documents/frs-tech-h9.pdf';
 import socioH9 from '../../assets/documents/frs-socio-h9.pdf';
+import techH10 from '../../assets/documents/frs-tech-h10.pdf'
 import FileButton from './FileButton';
 import innovationVideo from '../../assets/backgrounds/innovation-video.mov';
 import style from './style.module.scss';
@@ -41,6 +42,15 @@ export default function ResearchPage() {
                     fileDesc="AI based analysis of the public discourse of hyperloop and guidelines for optimal divulgation"
                     fileName="SocioEconomical FRS H9.pdf"
                     fileURL={socioH9}
+                    viewportParameters={{ scale: 1.0 }}
+                    previewWidth={210 * 2}
+                    previewHeight={297 * 2}
+                />
+                <FileButton
+                    fileTitle="Technical FRS H10"
+                    fileDesc="Comparative analysis of energy supply systems for hyperloop vehicle implementation"
+                    fileName="Technical FRS H10.pdf"
+                    fileURL={techH10}
                     viewportParameters={{ scale: 1.0 }}
                     previewWidth={210 * 2}
                     previewHeight={297 * 2}
